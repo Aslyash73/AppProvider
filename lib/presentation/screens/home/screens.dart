92af92a37586e1a0bc49ad7fb696b5a4e0358bc6
Asly Cantillo <acantill37@cuc.edu.co>
2023-05-26 15:31:21 -0500

    go_router_configurado

--- lib/presentation/screens/home/screens.dart
+++ lib/presentation/screens/home/screens.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_item.dart';
 import 'package:widgets_app/presentation/screens/buttons/screen.dart';
 
@@ -54,7 +55,8 @@ class _CustomListTile extends StatelessWidget {
         //     builder: (context) => const ButtonsScreen(),
         //   ),
         // );
-        Navigator.pushNamed(context, menuItem.link);
+        // Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
       },
     );
   }
